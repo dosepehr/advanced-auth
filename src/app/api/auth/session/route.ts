caddd1d21cabcc543d770feececc24bd2edd789b
dosepehr <dosepehr@gmail.com>
2025-09-22 12:50:16 +0330

    edit route handler

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -1,9 +1,21 @@
 import { decryptSession } from '@/utils/funcs/session';
 import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 
 export async function GET() {
     const cookieStore = await cookies();
     const encryptedSession = cookieStore.get('adv-session')?.value || '';
+
+    if (!encryptedSession) {
+        return NextResponse.json(
+            {
+                error: 'Session not found',
+            },
+            {
+                status: 400,
+            }
+        );
+    }
     const session = await decryptSession(encryptedSession);
     return Response.json(session);
 }
