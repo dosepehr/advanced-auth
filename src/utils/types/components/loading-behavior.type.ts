c5ea7d124ca7bb808aeb74372f6a0c253746a526
dosepehr <dosepehr@gmail.com>
2025-09-24 07:33:44 +0330

    add types

---
+++ src/utils/types/components/loading-behavior.type.ts
@@ -0,0 +1,15 @@
+import { ComponentBase } from './component-base.type';
+
+export type LoadingBehavior = ComponentBase & {
+    isLoading?: boolean;
+    loadingText?: string;
+    loadingType?: LoadingType;
+};
+
+export type LoadingType =
+    | 'spinner'
+    | 'dots'
+    | 'ring'
+    | 'ball'
+    | 'bars'
+    | 'infinity';
